865ebac7e9ab982528c0a3cb3679cacf1666f480
codeduchy <codeduchy@gmail.com>
2023-10-12 11:52:32 +0100

    modal setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,21 @@
+"use client";
+
+import { useStoreModal } from "@/hooks/use-store-modal";
 import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
+import { useEffect } from "react";
 
 export default function RootPage() {
+  const { onOpen, isOpen } = useStoreModal((state) => state);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
   return (
     <div className="p-4">
-      Home
       <UserButton afterSignOutUrl="/" />
     </div>
   );
