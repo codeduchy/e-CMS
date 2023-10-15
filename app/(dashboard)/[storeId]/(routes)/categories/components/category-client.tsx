d9966fec9151d69d8b0ec0576b33cc2906ca23b0
codeduchy <codeduchy@gmail.com>
2023-10-15 15:37:45 +0100

    categories

---
+++ app/(dashboard)/[storeId]/(routes)/categories/components/category-client.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import Heading from "@/components/heading";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { Billboard } from "@prisma/client";
+import { Plus } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+import { CategoryColumn, columns } from "./columns";
+import { DataTable } from "@/components/data-table";
+import ApiList from "@/components/api-list";
+
+type CategoryClientProps = {
+  data: CategoryColumn[];
+};
+
+const CategoryClient: React.FC<CategoryClientProps> = ({ data }) => {
+  const router = useRouter();
+  const params = useParams();
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Categories (${data.length})`}
+          description="Manage categories for your store"
+        />
+        <Button
+          onClick={() => router.push(`/${params.storeId}/categories/new`)}
+        >
+          <Plus className="mr-2 h-4 w-4" />
+          Add New
+        </Button>
+      </div>
+      <Separator />
+      <DataTable columns={columns} data={data} searchKey="name" />
+      <Heading title="API" description="API calls for Categories" />
+      <Separator />
+      <ApiList entityName="categories" entityIdName="categoryId" />
+    </>
+  );
+};
+
+export default CategoryClient;
