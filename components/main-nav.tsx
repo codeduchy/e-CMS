e037f96cb271e56c77ebb409f36541d2c411a718
codeduchy <codeduchy@gmail.com>
2023-10-31 17:15:30 +0000

    stripe

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -42,6 +42,11 @@ const MainNav = ({
       label: "Products",
       active: pathname === `/${params.storeId}/products`,
     },
+    {
+      href: `/${params.storeId}/orders`,
+      label: "Orders",
+      active: pathname === `/${params.storeId}/orders`,
+    },
   ];
 
   return (
--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export const middleware = authMiddleware({
-  // publicRoutes: ["/api/:path"],
+  publicRoutes: ["/api/:path*"],
 });
 
 export const config = {
